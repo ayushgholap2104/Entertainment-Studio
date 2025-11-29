108ff90a682bb3101930cc3fc945ea679d783ec0
ayushgholap2104 <ayushgholap2104@gmail.com>
2025-11-29 21:37:36 +0530

    Latest Update

--- js/project.js
+++ js/project.js
@@ -14,6 +14,7 @@ window.addEventListener("load", () => {
   upButton()
   toggleTheme()
   card_right_slide()
+  feedback_msg()
 
 })
 
@@ -702,6 +703,33 @@ function card_right_slide(){
     })
   });
 }
+
+const form = document.getElementById('my_form')
+async function handleSubmit(event){
+  event.preventDefault()
+  const status = document.querySelector('.popup-center')
+  var data =new FormData(event.target);
+
+  fetch(event.target.action,{
+    method:form.method,
+    body:data,
+    headers:{'accept':'application/json'},
+  }).then(response =>{
+    if(response.ok){
+      status.classList.add('active')
+      form.reset()
+    }else{
+      status.innerHTML = 'Oops! There Was a Problem Submitting Your Form'
+    }
+  }).catch(error =>{
+    status.innerHTML = 'Oops! There Was a Problem Submitting Your Form'
+  });
+}
+form.addEventListener('submit',handleSubmit)
+const feedback_close_btn = document.querySelector('#popup-btn-close')
+feedback_close_btn.addEventListener('click',()=>{
+  document.getElementById('status').classList.remove('active')
+})
 
 
 
