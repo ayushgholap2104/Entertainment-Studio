e534e3e565e12261b5176192b98b1f909ec94ddc
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-04-01 18:22:20 +0530

    Updated Code

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,17 +1,59 @@
 const db = require("../config/db")
+const bcrypt = require("bcrypt")
 
-exports.signup = (req,res)=>{
-  const { name, email, password} = req.body
+exports.signup = (req, res) => {
+  const { name, email, password } = req.body
 
   db.query(
-    "INSERT INTO users_detail (name,email,password) VALUES (?,?,?)",
-    [name, email, password],
-    (err,result) =>{
+    "SELECT * FROM users_detail WHERE email = ?",
+    [email],
+    async (err, result) => {
+      if (err) {
+        return res.status(500).json(err)
+      }
+      if (result.length > 0) {
+        return res.json("Email is already exist")
+      }
+      try {
+        const hashedpassword = await bcrypt.hash(password,10)
+        db.query(
+          "INSERT INTO users_detail (name,email,password) VALUES (?,?,?)",
+          [name, email, hashedpassword],
+          (err, result) => {
+            if (err) {
+              return res.status(500).json(err)
+            }
+            res.json("User registered successfully")
+          }
+        )
+      } catch (error) {
+        return res.status(500).json("Error hashing password")
+      }
+    }
+  )
+}
+exports.login = (req,res) =>{
+  const {email,password} = req.body
+  db.query(
+    "SELECT * FROM users_detail WHERE email = ?",
+    [email],
+    async (err,result) =>{
       if (err){
         return res.status(500).json(err)
       }
-
-      res.json("User registered successful")
+      if (result.length ===0){
+        return res.json("User does not exist")
+      }
+      const user = result[0]
+      try {
+        const isMatch = await bcrypt.compare(password,user.password)
+        if (!isMatch){
+          return res.json("Incorrect password")
+        }
+        res.json("Login successful")
+      } catch (error) {
+        res.status(500).json("Error in comparing password")
+      }
     }
   )
 }
