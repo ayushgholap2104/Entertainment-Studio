54e325c0abcbe4e873754f12a59cb15d99cc91ec
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-04-11 19:02:50 +0530

    Updated Code

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,13 @@
-// window.addEventListener("load", () => {
-//   loginData()
-// })
+
+function showToast(message, type = "success") {
+  const toastEl = document.getElementById("toastMsg");
+  const toastText = document.getElementById("toastText");
+
+  toastText.innerText = message;
+
+  // Change color
+  toastEl.className = `toast align-items-center text-bg-${type} border-0`;
+
+  const toast = new bootstrap.Toast(toastEl);
+  toast.show();
+}
