4e1945f2c50f064352e747d5403aebe2c1f4cd13
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-05-29 18:03:00 +0530

    Updated Code

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
   // profileData()
 })
 
-function sidebarClick(){
+function sidebarClick() {
   const sidebarIcon = document.querySelector('.sidebar')
   const sidebarMenu = document.querySelector('#side-menu')
 
@@ -14,22 +14,35 @@ function sidebarClick(){
   })
 }
 
-function profilePopup(){
+function profilePopup() {
   const userProfilebtn = document.querySelector('.language')
   const showProfile = document.querySelector('.profile_popup')
-  userProfilebtn.addEventListener('click',()=>{
+  userProfilebtn.addEventListener('click', () => {
     showProfile.classList.toggle('active');
     userProfilebtn.classList.toggle('active');
-    
   })
 }
+// const profile_form = document.querySelector('.profile_form')
+// function profileDelete_modal(event) {
+//   event.preventDefault()
+//   const profileDelete = document.querySelector('#user_deleteAccount')
+//   const modalPopup = document.querySelector('.profile_warning_msg')
+//   const cancleModal = document.querySelector('#profile-btn-cancle')
+//   profileDelete.addEventListener('click', () => {
+//     modalPopup.classList.toggle('active')
+//   })
 
-async function profileData(){
+//   cancleModal.addEventListener('click', () => {
+//     modalPopup.classList.remove('active')
+//   })
+// }
+
+async function profileData() {
   const token = localStorage.getItem("token")
 
-  const res = await fetch("http://localhost:5000/api/auth/profile",{
-    headers:{
-      "authorization":token
+  const res = await fetch("http://localhost:5000/api/auth/profile", {
+    headers: {
+      "authorization": token
     }
   })
   const data = await res.json()
